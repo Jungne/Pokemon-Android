65d95e7878d04c68977e793bdba74e584b75f5a6
Jungne <jungne7@gmail.com>
2019-04-09 14:29:28 +0200

    Added simple webcall
    Picks a random number between 1-151 and shows the name of the pokemon with that ID.

--- app/src/main/java/com/example/pokemonandroid/MainActivity.java
+++ app/src/main/java/com/example/pokemonandroid/MainActivity.java
@@ -1,13 +1,76 @@
 package com.example.pokemonandroid;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.JsonReader;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Random;
+
+import utilities.NetworkUtils;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button testButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button testButton = findViewById(R.id.testButton);
+        testButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String pokeID = String.valueOf((int)(Math.random() * 151) + 1);
+                URL url = NetworkUtils.buildUrl(pokeID);
+                new PokeAPIQueryTask().execute(url);
+            }
+        });
+    }
+
+    public class PokeAPIQueryTask extends AsyncTask<URL, Void, String> {
+        @Override
+        protected String doInBackground(URL... urls) {
+            URL searchUrl = urls[0];
+            String pokeAPIQueryResults = null;
+
+            try {
+                pokeAPIQueryResults = NetworkUtils.getResponseFromHttpUrl(searchUrl);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return pokeAPIQueryResults;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            if (s != null && !s.equals("")) {
+                JSONObject pokemon = createJSONObjectFromString(s);
+                TextView textView = findViewById(R.id.text);
+                try {
+                    textView.setText((String)pokemon.get("name"));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private JSONObject createJSONObjectFromString(String s) {
+        JSONObject obj = null;
+        try {
+            obj = new JSONObject(s);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return obj;
     }
 }
